fb224e88007a980efb71564a0a1ca29cc3977456
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-07-20 16:19:22 +0100

    Added functionality to test for filling and emptying of a water bottle.

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -9,4 +9,16 @@ public class WaterBottle {
         return volume;
     }
 
+    public void drink() {
+        this.volume = this.volume - 10;
+    }
+
+    public void empty() {
+        this.volume = 0;
+    }
+
+    public void fill() {
+        this.volume = 100;
+    }
+
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -4,19 +4,35 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class WaterBottleTest {
-    private WaterBottle waterbottle;
+    private WaterBottle waterBottle;
 
     @Before
     public void before() {
-        waterbottle = new WaterBottle(100);
+        waterBottle = new WaterBottle(100);
     }
 
     @Test
-    public void hasVolume() {
-        assertEquals(100, waterbottle.volume());
+    public void canGetVolume() {
+        assertEquals(100, waterBottle.volume());
     }
 
-    
+    @Test
+    public void canDrink() {
+        waterBottle.drink();
+        assertEquals(90, waterBottle.volume());
+    }
+
+    @Test
+    public void canEmptyBottle() {
+        waterBottle.empty();
+        assertEquals(0, waterBottle.volume());
+    }
+
+    @Test
+    public void canFillBottle() {
+        waterBottle.fill();
+        assertEquals(100, waterBottle.volume());
+    }
 
 
 
