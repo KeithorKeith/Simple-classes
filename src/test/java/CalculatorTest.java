a7383fd1447cfd660e79519ad6f6c0636681cb19
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-07-20 15:43:28 +0100

    Calculator complete.

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    private Calculator calculator;
+
+    @Before
+    public void before() {
+        this.calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd() {
+        assertEquals(10, calculator.add(5, 5));
+    }
+
+    @Test
+    public void canSubtract() {
+        assertEquals(0, calculator.subtract(5, 5));
+    }
+
+    @Test
+    public void canMultiply() {
+        assertEquals(25, calculator.multiply(5, 5));
+    }
+
+    @Test
+    public void canDivide() {
+        assertEquals(1.2, calculator.divide(6, 5), 0.01);
+    }
+
+
+}
